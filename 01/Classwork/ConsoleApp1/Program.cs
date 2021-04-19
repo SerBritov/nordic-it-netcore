2f698b2fd9f135998dde84caf0a99c5df1582b56
asncn <asncn@mail.ru>
2021-04-19 22:34:56 +0300

    Class work #1 added

---
+++ 01/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+			Console.WriteLine("Имя");
+						
+			string name;
+			name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Здравствуйте, {name}!");
+			/*Thread.Sleep(1000);
+			Console.WriteLine("Хотите еще кого-нибудь поприветствовать?y/n");
+			if Console.ReadKey() == "y" */
+				
+
+
+		}
+	}
+}
