4d8888b953191a7d826ec78f6a1d9e54cf7dc7fa
SerBritov <serbritov@yandex.ru>
2021-05-19 00:12:27 +0300

    Homework#6 added

---
+++ 06/Homework06/Homework06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Homework06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            uint userNumber = 0;
+            byte evenNumbers = 0;
+            Console.Write("Enter your natural number less than 2 billions: ");
+            do
+            {
+                userInput = Console.ReadLine();
+                try
+                {
+                    userNumber = uint.Parse(userInput);
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e + "! Try again: ");
+                    continue;
+                }
+                if (userNumber > 2000000)
+                    Console.Write("System.OverflowException! Try again: ");
+                else if (userNumber == 0)
+                    Console.WriteLine("Not natural number! Try again: ");
+                else
+                    break;
+            } while (true);
+
+            foreach (var number in userInput)
+            {
+                if ((number - '0') % 2 == 0)
+                    evenNumbers++;
+            }
+
+            Console.WriteLine($"Your number {userInput} have {evenNumbers} even numbers");
+        }
+    }
+}
---
+++ 06/Homework06/Homework06_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework06_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float contribution = 0;
+            float accumulationPercentage = 0;
+            float desiredOutput = 0;
+            ushort daysToWait = 0;
+
+            UserInputFloatParsing("Enter your contribution: ", out contribution);
+            UserInputFloatParsing("Enter accumulation percentage: ", out accumulationPercentage);
+            UserInputFloatParsing("Enter desired output: ", out desiredOutput);
+            
+            for (float moneyThatDay = contribution;
+                moneyThatDay < desiredOutput;
+                daysToWait++)
+                moneyThatDay *= (1f + accumulationPercentage / 100f);
+
+            Console.WriteLine($"Days until you get desired value: {daysToWait}");
+
+        }
+        static void UserInputFloatParsing(string s, out float inputed)
+        {
+            do
+            {
+                Console.Write(s);
+                try
+                {
+                    inputed = float.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error input! Try again.");
+                    continue;
+                }
+                break;
+            } while (true);
+        }
+    }
+}
