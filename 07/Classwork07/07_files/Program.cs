ec2d6c2b98a1da40e5f0bfa48aed37598b32d61f
SerBritov <serbritov@yandex.ru>
2021-05-25 19:07:01 +0300

    Classwork #7 added

---
+++ 07/Classwork07/07_files/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _07_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Работа с файлами
+             */
+
+            /*
+             * 
+             */
+            const string fileName = @"test.txt";    //если не указан путь, то файл лежит там, где находится исполняемый файл
+            const string fileName1 = @".\..\..\..\test.txt"; // ".\ --текущая директория, "..\" на уровень выше
+            //относительный путь -- исполняемый файл является точкой отсчета
+            string testMessage = "Hello, world";
+
+            //текст нужно подготовить как массив байтов, т.к. кодировка текста возможна различная
+            byte[] messageAnsiBytes = Encoding.ASCII.GetBytes(testMessage);
+
+            //Запись информации в файл
+            File.WriteAllBytes(fileName, messageAnsiBytes);
+            File.WriteAllBytes(fileName1, messageAnsiBytes);
+
+            //чтение
+            const string fileNameToRead = @".\..\..\..\read.txt";
+            string fileContent = File.ReadAllText(fileNameToRead);
+            Console.WriteLine(fileContent);
+
+            Directory.CreateDirectory(@"d:\test");  //создание директории
+            Directory.CreateDirectory(@"d:\test\test");
+        }
+    }
+}
