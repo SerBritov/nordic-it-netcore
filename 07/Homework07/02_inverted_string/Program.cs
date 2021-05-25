dceec266599406f3d6720af8b5821a65ef9543e4
SerBritov <serbritov@yandex.ru>
2021-05-25 19:07:41 +0300

    Homework #7 added

---
+++ 07/Homework07/02_inverted_string/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_inverted_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Enter non-empty string");
+                try
+                {
+                    input = Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("String seems too long");
+                    continue;
+                }
+                string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (words.Length > 0)
+                    break;
+                Console.WriteLine("Empty string");
+            } while (true);
+
+            for (int i = 0; i < input.Length; i++)
+                Console.Write(input[input.Length - 1 - i]);
+        }
+    }
+}
---
+++ 07/Homework07/Homework07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Homework07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string[] words;
+            char symbolToCount = 'a';
+            do
+            {
+                Console.WriteLine("Enter at least 2 words");
+                try
+                {
+                    input = Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("String seems too long");
+                    continue;
+                }
+                words = input.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (words.Length > 1)
+                    break;
+                Console.WriteLine("Not enough words");
+            } while (true);
+
+            int aCounter = 0;
+            foreach (var word in words)
+            {
+                if (word[0] == symbolToCount)
+                    aCounter++;
+            }
+
+            Console.WriteLine($"Count of words starting with 'a': {aCounter}");
+        }
+    }
+}
