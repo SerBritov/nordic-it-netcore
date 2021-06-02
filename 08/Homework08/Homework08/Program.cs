8c0f23ebbda36cf566d8f17c7906cb94c907bef5
SerBritov <serbritov@yandex.ru>
2021-06-02 16:45:17 +0300

    Homework #9 added

--- 08/Homework08/Homework08/Program.cs
+++ 08/Homework08/Homework08/Program.cs
@@ -24,16 +24,18 @@ namespace Homework08
             }
 
             int initialLength;
-            do
+            do      /*в наихудшем случае каждый цикл будет уменьшать количество символов в строке на 2 и будет выполняться N/2 раз. 
+                     *В наилучшем (?) -- на 6, и будет выполняться N/6 раз
+                     */
             {
                 initialLength = input.Length;
-                foreach (var bracketPair in brackets)
+                foreach (var bracketPair in brackets)   //3, т.к. три пары ключей
                 {
-                    for (int i = input.IndexOf(bracketPair.Key);
+                    for (int i = input.IndexOf(bracketPair.Key);    //N/2, линейная зависимость от количества символов в строке. Наилучший (?) случай -- N/3
                         i > -1 && i < input.Length - 1;
                         )
                     {
-                        if (input[i + 1] == bracketPair.Value && input[i] == bracketPair.Key)
+                        if (input[i + 1] == bracketPair.Value && input[i] == bracketPair.Key)   
                         {
                             input = input.Remove(i, 2);
                             i = input.IndexOf(bracketPair.Key);
@@ -50,3 +52,9 @@ namespace Homework08
     }
 }
 
+/*Все прочие операции не зависят от количества входных данных. 
+ * В наихудшем случае -- (N/2)*3*(N/2) = (3/4)N^2 и сводится к порядку N^2;
+ * в наилучшем (если входная строка написана правильно) случае (N/6)*3*(N/3) = (1/6)N^2, что также сводится к порядку N^2. 
+ * Если за наилучший случай принимать условие, что строка написана неправильно, 
+ * то сложность зависит только от внутреннего цикла for и сводится к порядку N^1.
+ */
