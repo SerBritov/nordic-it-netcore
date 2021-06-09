fe6f38d4a164d31406caf3f493b44f526ed0e709
asncn <asncn@mail.ru>
2021-06-09 18:12:22 +0300

    Homework #11 added

---
+++ 11/Homework11/Homework11/Alarm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    class Alarm
+    {
+        public DateTimeOffset AlarmDate;
+        public string AlarmMessage;
+        public TimeSpan TimeToAlarm
+        {
+            get
+            { return AlarmDate - DateTimeOffset.Now; }
+            set { }
+        }
+        public bool IsOutdated
+        {
+            get
+            {
+                return (TimeToAlarm < TimeSpan.FromMilliseconds(0));
+            }
+            set { }
+        }
+
+        public Alarm(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine($"Alarm date: {AlarmDate}\nAlarm message: {AlarmMessage}\nTime to alarm: {TimeToAlarm}\nIsOutdated: {IsOutdated}");
+        }
+    }
+}
---
+++ 11/Homework11/Homework11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Alarm alarm1 = new Alarm(DateTimeOffset.Now + TimeSpan.FromHours(5), "5-hours alarm");
+            Alarm alarm2 = new Alarm(DateTimeOffset.Now - TimeSpan.FromHours(5), "5-hours outdated alarm");
+            alarm1.WriteProperties();
+            alarm2.WriteProperties();
+            Console.ReadKey();
+        }
+    }
+}
