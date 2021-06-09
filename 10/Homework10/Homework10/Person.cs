370c0c64f691660c532dd259096be19ead1599fe
asncn <asncn@mail.ru>
2021-06-09 18:12:05 +0300

    Homework #10 added

---
+++ 10/Homework10/Homework10/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    class Person
+    {        
+        public string Name;
+        public byte Age;
+        public byte SomeYears
+        {
+            get
+            {
+                return (byte)4;
+            }
+            set { }
+        }
+        public byte AgeInSomeYears
+        {
+            get { return (byte)(Age + SomeYears); }
+            set { }
+        }
+    }
+}
---
+++ 10/Homework10/Homework10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte length = 3;
+            Person[] persons = new Person[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                persons[i] = new Person();
+                Console.Write("Enter name: ");
+                persons[i].Name = Console.ReadLine();
+                Console.Write($"Enter {persons[i].Name}'s age: ");
+                persons[i].Age = (byte)(IntInput());
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"Name: {persons[i].Name}; age in {persons[i].SomeYears} years: {persons[i].AgeInSomeYears}");
+            }
+            Console.ReadKey();
+        }
+
+        static int IntInput()
+        {
+            int result;
+            do
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Use only digits! Try again.");
+                    continue;
+                }
+            while (true);
+            return result;
+        }
+    }
+}
