0049b651da810312ac1214e10cb23926c149a21a
asncn <asncn@mail.ru>
2021-06-09 22:11:04 +0300

    Homework #12 added

--- 12/Classwork12/01_Selfwork/Passport.cs
+++ 12/Classwork12/01_Selfwork/Passport.cs
@@ -4,50 +4,56 @@ using System.Text;
 
 namespace _01_Selfwork
 {
-    public class Passport : BaseDocument 
-    {
-        public string Country;
-        public string PersonName;
+	public class Passport : BaseDocument
+	{
+		public string Country;
+		public string PersonName;
 
-        public Passport(string number, DateTimeOffset issueDate, string country, string personName): base("Passport", number, issueDate)
-        {
-            Country = country;
-            PersonName = personName;
-        }
+		public Passport(string number, DateTimeOffset issueDate, string country, string personName) : base("Passport", number, issueDate)
+		{
+			Country = country;
+			PersonName = personName;
+		}
 
 
-        public override string PropertiesString
-        { get 
-            {
-                return
-               $"Name: {DocName}; Number: {DocNumber}; Issue date: {IssueDate.ToString()}; Country: {Country}; PersonName: {PersonName}";
-            }
-            set { }
-        }
+		public override string PropertiesString
+		{
+			get
+			{
+				return
+			   $"Name: {DocName}; Number: {DocNumber}; Issue date: {IssueDate.ToString()}; Country: {Country}; PersonName: {PersonName}";
+			}
+			set { }
+		}
 
-        //new public void WriteToConsole()
-        //{
-        //    Console.WriteLine(PropertiesString);
-        //}
+		//new public void WriteToConsole()
+		//{
+		//    Console.WriteLine(PropertiesString);
+		//}
 
-        public void SetProperties(string name, string number, DateTimeOffset issueDate, string country, string  person)
-        {
-            DocName = name;
-            DocNumber = number;
-            IssueDate = issueDate;
-            Country = country;
-            PersonName = person;
-        }
+		public void SetProperties(string name, string number, DateTimeOffset issueDate, string country, string person)
+		{
+			DocName = name;
+			DocNumber = number;
+			IssueDate = issueDate;
+			Country = country;
+			PersonName = person;
+		}
 
-        public override bool Equals(object passport)
-        {
-            return /*passport.ToString() == "_01_Selfwork.Passport"*/
-                (passport is Passport) &&
-                ((Passport)passport).Country == Country &&
-                ((Passport)passport).DocName == DocName &&
-                ((Passport)passport).DocNumber == DocNumber &&
-                ((Passport)passport).IssueDate == IssueDate &&
-                ((Passport)passport).PersonName == PersonName;
-        }
-    }
+		public override bool Equals(object passport)
+		{
+			return /*passport.ToString() == "_01_Selfwork.Passport"*/
+				(passport is Passport) &&
+				((Passport)passport).Country == Country &&
+				((Passport)passport).DocName == DocName &&
+				((Passport)passport).DocNumber == DocNumber &&
+				((Passport)passport).IssueDate == IssueDate &&
+				((Passport)passport).PersonName == PersonName;
+		}
+
+		public void ChangeIssueDate(DateTimeOffset newIssueDate)
+		{
+			IssueDate = newIssueDate;
+		}
+	}
 }
--- 12/Classwork12/01_Selfwork/Program.cs
+++ 12/Classwork12/01_Selfwork/Program.cs
@@ -2,28 +2,44 @@
 
 namespace _01_Selfwork
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            var passport2 = new Passport("124253", DateTimeOffset.Parse("12.01.2020"), "Russia", "Sergei");
-            var passport3 = new Passport("124253", DateTimeOffset.Parse("12.01.2020"), "Russia", "Sergei");
-            
-            //passport2.SetProperties("name", "12421354", DateTimeOffset.Parse("12.01.2020"), "Russia", "Sergei");
+	class Program
+	{
+		static void Main(string[] args)
+		{
 
-           
-            passport2.WriteToConsole();
-            Console.WriteLine(passport2.Equals(passport3));
+			var passport2 = new Passport("124253", DateTimeOffset.Parse("12.01.2020"), "Russia", "Sergei");
+			var passport3 = new Passport("124253", DateTimeOffset.Parse("12.01.2020"), "Russia", "Sergei");
 
-            var doc1 = new BaseDocument();
-            var doc2 = new BaseDocument();
-            doc1.SetProperties("name", "12421354", DateTimeOffset.Parse("12.01.2020"));
-            doc1.WriteToConsole();
-            doc2.SetProperties("name", "12421354", DateTimeOffset.Parse("12.01.2020"));
+			//passport2.SetProperties("name", "12421354", DateTimeOffset.Parse("12.01.2020"), "Russia", "Sergei");
 
-            Console.WriteLine(doc1.Equals(doc2));
-        }
-    }
+
+			passport2.WriteToConsole();
+			Console.WriteLine(passport2.Equals(passport3));
+
+			var doc1 = new BaseDocument();
+			var doc2 = new BaseDocument();
+			doc1.SetProperties("name", "12421354", DateTimeOffset.Parse("12.01.2020"));
+			doc1.WriteToConsole();
+			doc2.SetProperties("name", "12421354", DateTimeOffset.Parse("12.01.2020"));
+
+			Console.WriteLine(doc1.Equals(doc2));
+			int length = 4;
+			BaseDocument[] someDoc = new BaseDocument[length];
+
+			someDoc[0] = passport2;
+			someDoc[1] = passport3;
+			someDoc[2] = doc1;
+			someDoc[3] = doc2;
+
+			foreach (var doc in someDoc)
+			{
+				if (doc is Passport)
+					((Passport)doc).ChangeIssueDate(DateTimeOffset.Now);
+			}
+
+			foreach (var doc in someDoc)
+				doc.WriteToConsole();
+		}
+	}
 }
 
