f447b5864a26e34a579f60d8852b141730cff1a3
SerBritov <serbritov@yandex.ru>
2021-06-23 00:01:26 +0300

    Homework #12 added

---
+++ 12/Homework12/Homework12/PhoneReminderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework12
+{
+	class PhoneReminderItem : ReminderItem
+	{
+		public string PhoneNumber { get; set; }
+		public PhoneReminderItem(string phoneNumber, DateTimeOffset _alarmDate, string _alarmMessage):base(_alarmDate, _alarmMessage)
+		{
+			PhoneNumber = phoneNumber;
+		}
+
+		public override void WriteProperties()
+		{
+			Console.Write($"Phone number: {PhoneNumber}; ");
+			base.WriteProperties();
+		}
+
+	}
+}
---
+++ 12/Homework12/Homework12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ReminderItem reminderItem = 
+				new ReminderItem(
+				DateTimeOffset.Now + TimeSpan.FromHours(8), 
+				"Alarm in 8 Hours");
+
+			ChatReminder chatReminder =
+				new ChatReminder(
+					"@sergei",
+					"Sergei",
+					DateTimeOffset.Now + TimeSpan.FromHours(12),
+					"Chat reminder in 12 hours");
+
+			PhoneReminderItem phoneReminder =
+				new PhoneReminderItem(
+					"+7890345235",
+					DateTimeOffset.Now + TimeSpan.FromHours(16),
+					"Phone alarm in 16 hours");
+
+			List<ReminderItem> ReminderList = new List<ReminderItem>();
+			ReminderList.Add(reminderItem);
+			ReminderList.Add(chatReminder);
+			ReminderList.Add(phoneReminder);
+
+			foreach (var reminder in ReminderList)
+				reminder.WriteProperties();
+		}
+	}
+}
