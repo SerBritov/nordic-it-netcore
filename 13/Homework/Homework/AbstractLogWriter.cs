1d43c924c732b1485c052428f85f6683614e6715
SerBritov <serbritov@yandex.ru>
2021-07-05 14:59:16 +0300

    Homework #13 added

---
+++ 13/Homework/Homework/AbstractLogWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    public abstract class AbstractLogWriter : ILogWriter
+    {
+        protected string LogRecordFormat
+        {
+            get
+            {
+                return "{0:yyyy-MM-dd hh:mm:ss.ffff+0000}: {1} - {2}";
+            }
+        }
+
+        private string GetLogRecord(string message, string logType)
+        {
+            return String.Format(LogRecordFormat, DateTimeOffset.Now, logType, message);
+        }
+
+        public virtual void LogInfo(string message)
+        {
+            LogRecordByType(GetLogRecord(message, "Info"));
+        }
+
+        public virtual void LogWarning(string message)
+        {
+            LogRecordByType(GetLogRecord(message, "Warning"));
+        }
+
+        public virtual void LogError(string message)
+        {
+            LogRecordByType(GetLogRecord(message, "Error"));
+        }
+                
+        abstract protected void LogRecordByType(string record);
+    }
+}
---
+++ 13/Homework/Homework/ConsoleLogWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    public class ConsoleLogWriter : AbstractLogWriter, ILogWriter
+    {
+
+        protected override void LogRecordByType(string record)
+        {
+            Console.WriteLine(record);
+        }
+    }
+}
